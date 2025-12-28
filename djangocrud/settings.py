1dce98ea565a7d2e23ec2ba2b83f063b4fac29fc
Marcos144-Pixel <camppozanomendoza@gmail.com>
2025-12-27 23:27:19 -0500

    Configurar CSRF y ALLOWED_HOSTS para Render

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -1,7 +1,6 @@
 """
 Django settings for djangocrud project.
 """
-
 from pathlib import Path
 import os
 #import dj_database_url
@@ -10,16 +9,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-desarrollo-local-123456789'
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Para desarrollo/pruebas
-# Cambia a False cuando esté todo funcionando bien
+SECRET_KEY = os.environ.get('SECRET_KEY', default='i83bweo!_8d1&rq#nfwd*0#8c5vq0oln1%io3fl0#2bgn&@rb(')
 
-ALLOWED_HOSTS = ['marcos-luis-campozano-mendoza.onrender.com', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = [
-    'https://marcos-luis-campozano-mendoza.onrender.com',
-    'http://marcos-luis-campozano-mendoza.onrender.com',
-]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'RENDER' not in os.environ
+
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -68,11 +63,12 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
+
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -96,11 +92,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_URL = '/signin'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
